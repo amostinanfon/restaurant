4799421250f5db39e80ab9ba43a4bdb9e64a96f9
amostinanfon <amostinanfon17@gmail.com>
2022-11-15 20:16:08 +0100

    first great commit navbar component

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import Footer from './Footer'
+import Navbar  from './Navbar'
+
+export const Layout = ({children}) => {
+  return (
+    <>
+        <Navbar/>
+        Layout
+        {children}       
+        <Footer/>
+    </>
+  )
+}
