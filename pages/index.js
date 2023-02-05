03062ec4a949d05e21af2a68a8356bad90463c97
= <=>
2023-02-05 23:02:28 +0100

    working on restaurant project responsiveness

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 import Featured from '../components/Featured';
 import PizzaList from '../components/PizzaList';
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
