8a4489b229ec92ee6c4b01e39a1eb0ae7b40f7f2
= <=>
2023-02-18 13:58:44 +0100

    all right on product component

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -21,7 +21,7 @@ const Product = () => {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{pizza.name}</h1>
-        <span className={styles.price}>${pizza.price[size]}</span>
+        <span className={styles.price}>FCFA {pizza.price[size]}</span>
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>CHOISIR LA TAILLE</h3>
         <div className={styles.sizes}>
