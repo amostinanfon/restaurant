cc264d5e35c06a0c49b99fcd267e3fa1759a9244
amostinanfon <amostinanfon17@gmail.com>
2023-03-18 00:37:15 +0100

    catching state cart 5

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 
 const PizzaCard = ({pizza}) => {
 
-  useSelector(state => console.log(state));
+  useSelector(state => console.log(state.products));
 
 
   return (
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -15,7 +15,7 @@ const Product = ({pizza}) => {
   const dispatch = useDispatch();
 
 
-  const cart = useSelector(state => console.log(state))
+  const cart = useSelector(state => console.log(state.products));
 
   const changePrice = (number) =>{
     setPrice(price + number);
